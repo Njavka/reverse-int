ceba515455e69f01bbbab39028c25be138198666
Navka <navka@list.ru>
2024-08-17 19:10:25 +0300

    feat: function written to return reversed integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let string = n.toString();
+  let stringRevert = string.split("").reverse().join("");
+  let numberRevert = parseInt(stringRevert, 10);
+  return numberRevert;
 }
